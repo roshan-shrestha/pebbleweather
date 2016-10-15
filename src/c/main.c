58d2e5ee020098499c63d5e1532c3e62c3977de6
Roshan Shrestha <rs1646@truman.edu>
2016-10-15 04:48:22 -0500

    added menu

--- src/c/main.c
+++ src/c/main.c
@@ -1,10 +1,18 @@
 #include <pebble.h>
 #include "splash_window.h"
+#include "main_window.h"
+
+void launch_main_window() {
+	window_stack_push(main_window_get_window(), true);
+}
 
 int main() {
-  app_event_loop();
-  
-  splash_window_create();
-  window_stack_push(splash_window_get_window(), true);
-  app_event_loop();
+	splash_window_create();
+	main_window_create();
+
+	window_stack_push(splash_window_get_window(), true);
+	app_timer_register(1500, launch_main_window, NULL);
+
+	app_event_loop();
+	splash_window_destroy();
 }
--- src/c/splash_window.c
+++ src/c/splash_window.c
@@ -6,25 +6,29 @@ BitmapLayer *splashImageLayer;
 GBitmap *splashImage;
 TextLayer *splashImageTitle;
 
-
 void splash_window_load(Window *window) {
 	
 	Layer *window_layer = window_get_root_layer(window);
 	
 	splashImage = gbitmap_create_with_resource(RESOURCE_ID_SPLASH_IMAGE);
+	
 	splashImageLayer = bitmap_layer_create(GRect(0, 0, 144, 120));
 	bitmap_layer_set_bitmap(splashImageLayer, splashImage);
 	bitmap_layer_set_compositing_mode(splashImageLayer, GCompOpSet);
 	layer_add_child(window_layer, bitmap_layer_get_layer(splashImageLayer));
 	
+	// Notice the pattern? Try gif someday.
 	splashImageTitle = text_layer_create(GRect(0, 115, 144, 58));
 	text_layer_set_text(splashImageTitle, "Weather Now");
 	text_layer_set_text_alignment(splashImageTitle, GTextAlignmentCenter);
 	layer_add_child(window_layer, text_layer_get_layer(splashImageTitle));
 }
 
+// Gotta clean up!
 void splash_window_unload(Window *window) {
-  
+	gbitmap_destroy(splashImage);
+	bitmap_layer_destroy(splashImageLayer);
+	text_layer_destroy(splashImageTitle);
 }
 
 void splash_window_create(){
