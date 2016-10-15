78f311ecfd080811afeeb8cacfd771d5416aa13b
Roshan Shrestha <rs1646@truman.edu>
2016-10-15 01:11:47 -0500

    added splash icon

--- src/c/splash_window.c
+++ src/c/splash_window.c
@@ -2,9 +2,19 @@
 #include "splash_window.h"
 
 Window *splashWindow;
+BitmapLayer *splashImageLayer;
+GBitmap *splashImage;
 
 void splash_window_load(Window *window) {
-  
+	
+	Layer *window_layer = window_get_root_layer(window);
+	
+	splashImage = gbitmap_create_with_resource(RESOURCE_ID_SPLASH_IMAGE);
+	splashImageLayer = bitmap_layer_create(GRect(0, 0, 144, 168));
+	bitmap_layer_set_bitmap(splashImageLayer, splashImage);
+	bitmap_layer_set_compositing_mode(splashImageLayer, GCompOpSet);
+	layer_add_child(window_layer, bitmap_layer_get_layer(splashImageLayer));
+	
 }
 
 void splash_window_unload(Window *window) {
